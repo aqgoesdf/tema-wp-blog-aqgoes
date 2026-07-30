9b7b516488b33e5b3f9dc4ce4dc42409efd403c1
AqGoEs <aqgoesdf@gmail.com>
2026-07-30 17:17:04 -0300

    Meta tag google search

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    
+      <meta name="google-site-verification" content="eTrlbtdq7bsM5xHgk9osh2-i7MqqCQRutgQ4kyutYys" />
     <?php wp_head(); ?>
 </head>
 
